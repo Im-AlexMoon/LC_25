bd5072216683370f27d43527114e42a687551217
Alexander Luna <A00574602@tec.mx>
2025-11-11 11:33:03 -0600

    Create leetcode problem architecture without my computer exploding

---
+++ src/problem_template.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+// Solution class for LeetCode problems
+class Solution {
+public:
+    // TODO: Implement your solution here
+    // Example: int twoSum(vector<int>& nums, int target) { ... }
+};
+
+// Main function for testing
+int main() {
+    Solution solution;
+
+    // TODO: Add test cases here
+    // Example:
+    // vector<int> nums = {2, 7, 11, 15};
+    // int target = 9;
+    // vector<int> result = solution.twoSum(nums, target);
+    // cout << "Result: " << result[0] << ", " << result[1] << endl;
+
+    cout << "Test case passed!" << endl;
+    return 0;
+}
