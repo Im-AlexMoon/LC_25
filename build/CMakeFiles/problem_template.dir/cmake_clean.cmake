d78f9564f5541327ee2a447a43c2773fc1942fb4
Alexander Luna <A00574602@tec.mx>
2025-11-12 16:40:20 -0600

    stage changed files

---
+++ build/CMakeFiles/problem_template.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/problem_template.dir/src/problem_template.cpp.obj"
+  "CMakeFiles/problem_template.dir/src/problem_template.cpp.obj.d"
+  "bin/problem_template.exe"
+  "bin/problem_template.exe.manifest"
+  "bin/problem_template.pdb"
+  "libproblem_template.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/problem_template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/two_sum.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/two_sum.dir/src/two_sum.cpp.obj"
+  "CMakeFiles/two_sum.dir/src/two_sum.cpp.obj.d"
+  "bin/two_sum.exe"
+  "bin/two_sum.exe.manifest"
+  "bin/two_sum.pdb"
+  "libtwo_sum.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/two_sum.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
